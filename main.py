edf2d3d2b185999c852abd5c0d5f3b2df1d2249e
Nksovreli <81826465+Nksovreli@users.noreply.github.com>
2022-09-07 14:29:44 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
 from fastapi import FastAPI
-
-
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+origins = [
+    "https://fastapi-ipove.herokuapp.com/",
+    "https://fastapi-ipove.herokuapp.com/",
+    "https://fastapi-ipove.herokuapp.com/",
+    "https://fastapi-ipove.herokuapp.com/",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=[""],
+    allow_headers=[""],
+)
 
 
-
-@app.get('/')
-async def home():
-    return {"data":"Hellow"}
-
-
-@app.get('/jobs')
-def jobs():
-    return {"msg":"jobs"}
+@app.get("/")
+async def main():
+    return {"message": "Hello World"}
