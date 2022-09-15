e8805f577059ede8d90392d15a5a68401f38a4a4
Nksovreli <81826465+Nksovreli@users.noreply.github.com>
2022-09-15 14:24:17 +0400

    1

--- auth2.py
+++ auth2.py
@@ -50,4 +50,4 @@ def get_current_user(token:str=Depends(ouath2_scheme),db: Session = Depends(data
     token = verify_access_token(token,credentials_exception)
     user = db.query(models.User).filter(models.User.id == token.id).first()
 
-    return user
+    return user.email
